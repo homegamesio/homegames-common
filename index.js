b0348eb7728dd78748db40d5eb8fe7fe04cf65ed
Joseph Garcia <prosif@gmail.com>
2023-08-22 15:49:23 -0700

    1.0.0

--- index.js
+++ index.js
@@ -723,6 +723,23 @@ const log = {
 
 }
 
+const getAppDataPath = () => {
+  switch (process.platform) {
+    case "darwin": {
+      return path.join(process.env.HOME, "Library", "Application Support", "homegames");
+    }
+    case "win32": {
+      return path.join(process.env.APPDATA, "homegames");
+    }
+    case "linux": {
+      return path.join(process.env.HOME, ".homegames");
+    }
+    default: {
+      console.log("Unsupported platform!");
+      process.exit(1);
+    }
+  }
+}
 
 module.exports = {
     signup,
@@ -737,6 +754,7 @@ module.exports = {
     guaranteeDir,
     getUrl,
     getConfigValue,
-    log
+    log,
+    getAppDataPath
 };
 
